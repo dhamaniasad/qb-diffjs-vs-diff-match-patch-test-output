f2cbe53d8322d14003eb5c4b018559bc90477b65
Asad Dhamani <dhamaniasad@gmail.com>
2022-02-27 13:45:39 +0530

    WIP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
-import { diffWordsWithSpace } from "diff";
+import { diffWordsWithSpace, diffWords } from "diff";
+import { diff_match_patch } from "diff-match-patch";
 
 const one =
     "In passive attack attacker generally, hide and collects data by tapping communication link but does not modify it.",
@@ -13,14 +14,24 @@ const diff = diffWordsWithSpace(one, other),
   display = document.getElementById("app"),
   fragment = document.createDocumentFragment();
 
+const differ = new diff_match_patch();
+
+const diffTwo = differ.diff_main(one, other);
+
 console.log("diff:", diff);
+
+console.log("diff two:", diffTwo);
+
+const differHtml = differ.diff_prettyHtml(diffTwo);
+
+document.querySelector("#app2").innerHTML += differHtml;
 
 diff.forEach((part) => {
   // green for additions, red for deletions
   // grey for common parts
-  const color = part.added ? "green" : part.removed ? "red" : "grey";
+  const color = part.added ? "limegreen" : part.removed ? "pink" : "white";
   span = document.createElement("span");
-  span.style.color = color;
+  span.style.backgroundColor = color;
   span.appendChild(document.createTextNode(part.value));
   fragment.appendChild(span);
 });
